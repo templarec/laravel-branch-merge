4d9a858d57d7465739b712118cf5dc08c882fa47
giuseppe <placidagiuseppe@gmail.com>
2021-06-01 16:23:20 +0200

    crud fish,db

---
+++ database/migrations/2021_06_01_141702_create_fish_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFishTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('fish', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->float('price');
+            $table->float('weight');
+            $table->string('race');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('fish');
+    }
+}
